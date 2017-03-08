3b3f5f6f0996f0fa5e7d6bffcef17e6a6cce575b
Hadrien G <knights_of_ni@gmx.com>
2017-03-08 19:34:18 +0100

    Moved asynchronous operation status related facilities to a (for now internal) module

--- src/main.rs
+++ src/main.rs
@@ -6,111 +6,118 @@
 
 extern crate triple_buffer;
 
-use std::error::Error;
-use std::fmt;
 use triple_buffer::TripleBuffer;
 
 
-/// Representation of an asynchronous operation's status
-///
-/// This enumeration is used to represent the status of an asynchronous
-/// operation. It follows a state machine design, with customization points
-/// so that each application can add whichever extra information it needs.
-///
-/// Here are the possible state transitions:
-///
-/// - Pending -> Pending' / Running / Cancelled / Error
-/// - Running -> Running' / Done / Cancelled / Error
-///
-/// Here, an apostrophe is used to denote a switch to another version of the
-/// same toplevel state with different implementation-specific details.
-///
-/// Note that once the asynchronous operation is in either of the Done, Error or
-/// Cancelled state, its state won't change anymore.
-///
-enum AsyncOpStatus<Details: AsyncOpStatusDetails> {
-    /// The request has been submitted, but not been processed yet
-    Pending(Details::PendingDetails),
+/// Facilities to represent the status of asynchronous operations
+mod status {
+    use std::error::Error;
+    use std::fmt;
 
-    /// The request is being processed by the server
-    Running(Details::RunningDetails),
+    /// Representation of an asynchronous operation's status
+    ///
+    /// This enumeration is used to represent the status of an asynchronous
+    /// operation. It follows a state machine design, with customization points
+    /// so that each application can add whichever extra information it needs.
+    ///
+    /// Here are the possible state transitions:
+    ///
+    /// - Pending -> Pending' / Running / Cancelled / Error
+    /// - Running -> Running' / Done / Cancelled / Error
+    ///
+    /// Here, an apostrophe is used to denote a switch to another version of the
+    /// same toplevel state with different implementation-specific details.
+    ///
+    /// Note that once the asynchronous operation is in either of the Done,
+    /// Error or Cancelled state, its state won't change anymore.
+    ///
+    #[derive(Clone, Debug, PartialEq)]
+    enum AsyncOpStatus<Details: AsyncOpStatusDetails> {
+        /// The request has been submitted, but not been processed yet
+        Pending(Details::PendingDetails),
 
-    /// The server has successfully processed the request
-    Done(Details::DoneDetails),
+        /// The request is being processed by the server
+        Running(Details::RunningDetails),
 
-    /// The client has cancelled the request before the server was done
-    Cancelled(Details::CancelledDetails),
+        /// The server has successfully processed the request
+        Done(Details::DoneDetails),
 
-    /// The server has failed to process the request
-    Error(Details::ErrorDetails),
-}
+        /// The client has cancelled the request before the server was done
+        Cancelled(Details::CancelledDetails),
 
-
-/// Implementation-specific details on the status of asynchronous operations
-trait AsyncOpStatusDetails {
-    /// Details on the status of pending operations. For example, OpenCL
-    /// distinguishes commands which are queued on the host and on the device.
-    type PendingDetails: Clone + PartialEq + Send;
-
-    /// Details on the status of running operations. A typical application is
-    /// tracking progress using unsigned integer counters.
-    type RunningDetails: Clone + PartialEq + Send;
-
-    /// Details on the status of completed operations. Can be used to store a
-    /// handle to operation results, for example.
-    type DoneDetails: Clone + PartialEq + Send;
-
-    /// Details on the status of cancelled operations. Can be used to provide
-    /// details on why a client has cancelled some operation.
-    type CancelledDetails: Clone + PartialEq + Send;
-
-    /// Details on the status of erronerous operations. Can be used by the
-    /// server to explain why it could not complete its work.
-    type ErrorDetails: Clone + PartialEq + Send + Error;
-}
-
-
-/// This struct, and the associated constant which represents the only value
-/// that it can take, can be used as a placeholder when the implementation does
-/// not need to add extra details to the asynchronous operation status.
-#[derive(Clone, Debug, PartialEq)]
-struct NoDetails {}
-//
-const NO_DETAILS: NoDetails = NoDetails {};
-//
-impl fmt::Display for NoDetails {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<No details>")
-    }
-}
-//
-impl Error for NoDetails {
-    fn description(&self) -> &str {
-        "<No error details>"
+        /// The server has failed to process the request
+        Error(Details::ErrorDetails),
     }
 
-    fn cause(&self) -> Option<&Error> {
-        None
+
+    /// Implementation-specific details on the status of asynchronous operations
+    trait AsyncOpStatusDetails {
+        /// Details on the status of pending operations. For example, OpenCL
+        /// distinguishes commands which are queued on the host vs the device.
+        type PendingDetails: Clone + PartialEq + Send;
+
+        /// Details on the status of running operations. A typical application
+        /// is tracking progress using unsigned integer counters.
+        type RunningDetails: Clone + PartialEq + Send;
+
+        /// Details on the status of completed operations. Can be used to store
+        /// a handle to operation results, for example.
+        type DoneDetails: Clone + PartialEq + Send;
+
+        /// Details on the status of cancelled operations. Can be used to
+        /// provide details on why a client has cancelled some operation.
+        type CancelledDetails: Clone + PartialEq + Send;
+
+        /// Details on the status of erronerous operations. Can be used by the
+        /// server to explain why it could not complete its work.
+        type ErrorDetails: Clone + PartialEq + Send + Error;
     }
-}
 
 
-/// When you don't need any implementation details, you can use this struct
-/// and the associated constants for extra writing convenience.
-type StandardAsyncOpStatus = AsyncOpStatus<NoDetails>;
-//
-const PENDING: StandardAsyncOpStatus = AsyncOpStatus::Pending(NO_DETAILS);
-const RUNNING: StandardAsyncOpStatus = AsyncOpStatus::Running(NO_DETAILS);
-const DONE: StandardAsyncOpStatus = AsyncOpStatus::Done(NO_DETAILS);
-const CANCELLED: StandardAsyncOpStatus = AsyncOpStatus::Cancelled(NO_DETAILS);
-const ERROR: StandardAsyncOpStatus = AsyncOpStatus::Error(NO_DETAILS);
-//
-impl AsyncOpStatusDetails for NoDetails {
-    type PendingDetails = NoDetails;
-    type RunningDetails = NoDetails;
-    type DoneDetails = NoDetails;
-    type CancelledDetails = NoDetails;
-    type ErrorDetails = NoDetails;
+    /// This struct, and the associated constant which represents the only value
+    /// that it can take, can be used as a placeholder when the implementation
+    /// does not need to add extra details to the asynchronous operation status.
+    #[derive(Clone, Debug, PartialEq)]
+    struct NoDetails {}
+    //
+    const NO_DETAILS: NoDetails = NoDetails {};
+    //
+    impl fmt::Display for NoDetails {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "<No details>")
+        }
+    }
+    //
+    impl Error for NoDetails {
+        fn description(&self) -> &str {
+            "<No error details>"
+        }
+
+        fn cause(&self) -> Option<&Error> {
+            None
+        }
+    }
+
+
+    /// When you don't need any implementation details, you can use this struct
+    /// and the associated constants for extra writing convenience.
+    type StandardAsyncOpStatus = AsyncOpStatus<NoDetails>;
+    //
+    const PENDING: StandardAsyncOpStatus = AsyncOpStatus::Pending(NO_DETAILS);
+    const RUNNING: StandardAsyncOpStatus = AsyncOpStatus::Running(NO_DETAILS);
+    const DONE: StandardAsyncOpStatus = AsyncOpStatus::Done(NO_DETAILS);
+    const CANCELLED: StandardAsyncOpStatus = AsyncOpStatus::Cancelled(
+        NO_DETAILS
+    );
+    const ERROR: StandardAsyncOpStatus = AsyncOpStatus::Error(NO_DETAILS);
+    //
+    impl AsyncOpStatusDetails for NoDetails {
+        type PendingDetails = NoDetails;
+        type RunningDetails = NoDetails;
+        type DoneDetails = NoDetails;
+        type CancelledDetails = NoDetails;
+        type ErrorDetails = NoDetails;
+    }
 }
 
 
