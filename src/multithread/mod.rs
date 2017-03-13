cca9e37100dfadab93acd73402c824460b50ad41
Hadrien G <knights_of_ni@gmx.com>
2017-03-13 12:49:34 +0100

    Extracted polling-based operation monitoring to a dedicated module

--- src/multithread/mod.rs
+++ src/multithread/mod.rs
@@ -17,100 +17,10 @@
 //!   synchronization mechanism, but it should be used with more care.
 
 
+mod polling;
+
 // TODO: Extract these modules to dedicated files
 // TODO: Add callback-based implementation
-
-
-/// Lock-free polling implementation of asynchronous operation monitoring
-///
-/// This implementation of the asynchronous operation concept is based on a
-/// triple buffer. It does not allow waiting for updates, instead only allowing
-/// the client to periodically poll the current operation status. But it may be
-/// the most efficient option for frequently updated operation statuses.
-///
-pub mod polling {
-    use server::{GenericAsyncOpServer, AsyncOpServerConfig};
-    use status::{AsyncOpStatus, AsyncOpStatusDetails};
-    use triple_buffer::{TripleBuffer, TripleBufferInput, TripleBufferOutput};
-
-
-    /// Asynchronous operation object
-    pub struct AsyncOp<Details: AsyncOpStatusDetails> {
-        /// Server interface used to submit status updates
-        server: AsyncOpServer<Details>,
-
-        /// Client interface used to monitor the operation status
-        client: AsyncOpClient<Details>,
-    }
-    //
-    impl<Details: AsyncOpStatusDetails> AsyncOp<Details> {
-        /// Create a new asynchronous operation object with some initial status
-        pub fn new(initial_status: AsyncOpStatus<Details>) -> Self {
-            // Keep a copy of the initial operation status
-            let initial_status_copy = initial_status.clone();
-
-            // Setup the client/server communication channel
-            let buffer = TripleBuffer::new(initial_status);
-            let (buf_input, buf_output) = buffer.split();
-
-            // ...then build the client and server
-            AsyncOp {
-                server: GenericAsyncOpServer::new(
-                    PollingServerConfig { buf_input: buf_input },
-                    &initial_status_copy
-                ),
-                client: AsyncOpClient { buf_output: buf_output },
-            }
-        }
-
-        /// Split the asynchronous operation object into client and server
-        /// objects which can be respectively sent to client and server threads
-        pub fn split(self) -> (AsyncOpServer<Details>, AsyncOpClient<Details>) {
-            (self.server, self.client)
-        }
-    }
-
-
-    /// Server interface, used to send operation status updates to the client
-    pub type AsyncOpServer<Details: AsyncOpStatusDetails> =
-        GenericAsyncOpServer<PollingServerConfig<Details>>;
-
-
-    /// Server configuration for polling-based operation monitoring
-    pub struct PollingServerConfig<Details: AsyncOpStatusDetails> {
-        /// New operation statuses will be sent through this triple buffer
-        buf_input: TripleBufferInput<AsyncOpStatus<Details>>,
-    }
-    //
-    impl<Details: AsyncOpStatusDetails> AsyncOpServerConfig
-        for PollingServerConfig<Details>
-    {
-        /// Implementation details of the asynchronous operation status
-        type StatusDetails = Details;
-
-        /// Method used to send a status update to the client
-        fn update(&mut self, status: AsyncOpStatus<Details>) {
-            self.buf_input.write(status);
-        }
-    }
-
-
-    /// Client interface, used to synchronize with the operation status
-    pub struct AsyncOpClient<Details: AsyncOpStatusDetails> {
-        /// Current operation status will be read through this triple buffer
-        buf_output: TripleBufferOutput<AsyncOpStatus<Details>>,
-    }
-    //
-    impl<Details: AsyncOpStatusDetails> AsyncOpClient<Details> {
-        /// Access the current asynchronous operation status
-        pub fn status(&mut self) -> &AsyncOpStatus<Details> {
-            self.buf_output.read()
-        }
-    }
-
-
-    // TODO: Add tests and benchmarks
-}
 
 
 /// Blocking implementation of asynchronous operation monitoring
---
+++ src/multithread/polling.rs
@@ -0,0 +1,89 @@
+//! Polling-based asynchronous operation monitoring
+//!
+//! This crates provides a way to monitor asynchronous operations through
+//! polling. It provides maximal performance in scenarios where a client does
+//! not need to synchronize with an asynchronous operation, but only to
+//! periodically check its status, as is the case for example when updating
+//! progress bars and status graphs in user interfaces.
+
+use server::{GenericAsyncOpServer, AsyncOpServerConfig};
+use status::{AsyncOpStatus, AsyncOpStatusDetails};
+use triple_buffer::{TripleBuffer, TripleBufferInput, TripleBufferOutput};
+
+
+/// Asynchronous operation object
+pub struct AsyncOp<Details: AsyncOpStatusDetails> {
+    /// Server interface used to submit status updates
+    server: AsyncOpServer<Details>,
+
+    /// Client interface used to monitor the operation status
+    client: AsyncOpClient<Details>,
+}
+//
+impl<Details: AsyncOpStatusDetails> AsyncOp<Details> {
+    /// Create a new asynchronous operation object with some initial status
+    pub fn new(initial_status: AsyncOpStatus<Details>) -> Self {
+        // Keep a copy of the initial operation status
+        let initial_status_copy = initial_status.clone();
+
+        // Setup triple buffer-based client/server communication...
+        let buffer = TripleBuffer::new(initial_status);
+        let (buf_input, buf_output) = buffer.split();
+
+        // ...then build the client and server
+        AsyncOp {
+            server: GenericAsyncOpServer::new(
+                PollingServerConfig { buf_input: buf_input },
+                &initial_status_copy
+            ),
+            client: AsyncOpClient { buf_output: buf_output },
+        }
+    }
+
+    /// Split the asynchronous operation object into client and server
+    /// objects which can be respectively sent to client and server threads
+    pub fn split(self) -> (AsyncOpServer<Details>, AsyncOpClient<Details>) {
+        (self.server, self.client)
+    }
+}
+
+
+/// Server interface, used to send operation status updates to the client
+pub type AsyncOpServer<Details: AsyncOpStatusDetails> =
+    GenericAsyncOpServer<PollingServerConfig<Details>>;
+
+
+/// Server configuration for polling-based operation monitoring
+pub struct PollingServerConfig<Details: AsyncOpStatusDetails> {
+    /// New operation statuses will be sent through this triple buffer
+    buf_input: TripleBufferInput<AsyncOpStatus<Details>>,
+}
+//
+impl<Details: AsyncOpStatusDetails> AsyncOpServerConfig
+    for PollingServerConfig<Details>
+{
+    /// Implementation details of the asynchronous operation status
+    type StatusDetails = Details;
+
+    /// Method used to send a status update to the client
+    fn update(&mut self, status: AsyncOpStatus<Details>) {
+        self.buf_input.write(status);
+    }
+}
+
+
+/// Client interface, used to synchronize with the operation status
+pub struct AsyncOpClient<Details: AsyncOpStatusDetails> {
+    /// Current operation status will be read through this triple buffer
+    buf_output: TripleBufferOutput<AsyncOpStatus<Details>>,
+}
+//
+impl<Details: AsyncOpStatusDetails> AsyncOpClient<Details> {
+    /// Access the current asynchronous operation status
+    pub fn status(&mut self) -> &AsyncOpStatus<Details> {
+        self.buf_output.read()
+    }
+}
+
+
+// TODO: Add tests and benchmarks
